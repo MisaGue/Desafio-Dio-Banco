b3b76ed567722d698f4cef0a4757f1cbaaa17ba5
Misael Guedes <107317200+MisaGue@users.noreply.github.com>
2022-08-03 20:52:16 -0300

    Update Iconta.java

--- src/Iconta.java
+++ src/Iconta.java
@@ -1,4 +1,11 @@
 
-public class Iconta {
-
+public interface IConta {
+	
+	void sacar(double valor);
+	
+	void depositar(double valor);
+	
+	void transferir(double valor, IConta contaDestino);
+	
+	void imprimirExtrato();
 }
