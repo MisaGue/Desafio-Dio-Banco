dad3c23b093536778c3d600c8583087c1a207630
Misael Guedes <107317200+MisaGue@users.noreply.github.com>
2022-08-03 21:16:08 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente misael = new Cliente();
+		venilton.setNome("Misael");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(misael);
+		Conta poupanca = new ContaPoupanca(misael);
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
